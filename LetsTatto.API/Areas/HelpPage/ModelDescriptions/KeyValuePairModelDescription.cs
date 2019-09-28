bcaf8872939f7b6273c637e2ea5800e44705f162
Enrique4to <48497762+Enrique4to@users.noreply.github.com>
2019-09-27 18:02:37 -0600

    Delete KeyValuePairModelDescription.cs

--- LetsTatto.API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace LetsTatto.API.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
